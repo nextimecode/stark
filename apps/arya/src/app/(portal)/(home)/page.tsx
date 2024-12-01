0a702dbc918ac3eb74eeb280e13f43b124453591
Pedro Duarte <pedrohlduarte@gmail.com>
2024-12-01 01:41:06 -0300

    feat: update button style and text to enhance user experience on home page

--- apps/arya/src/app/(portal)/(home)/page.tsx
+++ apps/arya/src/app/(portal)/(home)/page.tsx
@@ -22,9 +22,16 @@ export default function Home() {
   // const { message } = await getHomeData()
   return (
     <main>
-      <Button type="button" variant="link" isLoading>
-        Teste
-        </Button>
+      <Button
+        variant="solid"
+        color="blue"
+        size="large"
+        isFullWidth
+        startIcon={<svg>...</svg>}
+        endIcon={<svg>...</svg>}
+      >
+        Add to cart
+      </Button>
       {/* <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid md:grid-cols-2 gap-4 md:gap-8 xl:gap-20 md:items-center">
           <div>
