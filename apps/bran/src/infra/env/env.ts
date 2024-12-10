284a5d2a5d2cdc937b7524e44783d3be71a81a12
Pedro Duarte <pedrohlduarte@gmail.com>
2024-12-10 20:29:13 -0300

    feat: refactor application to use NestJS framework and update environment config for cloud services

--- apps/bran/src/infra/env/env.ts
+++ apps/bran/src/infra/env/env.ts
@@ -1,9 +1,17 @@
 import { z } from 'zod'
 
 export const envSchema = z.object({
-  NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
-  NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
-  PORT: z.coerce.number().optional().default(8000)
+  DATABASE_URL: z.string().url(),
+  JWT_PRIVATE_KEY: z.string(),
+  JWT_PUBLIC_KEY: z.string(),
+  CLOUDFLARE_ACCOUNT_ID: z.string(),
+  AWS_BUCKET_NAME: z.string(),
+  AWS_ACCESS_KEY_ID: z.string(),
+  AWS_SECRET_ACCESS_KEY: z.string(),
+  REDIS_HOST: z.string().optional().default('127.0.0.1'),
+  REDIS_PORT: z.coerce.number().optional().default(6379),
+  REDIS_DB: z.coerce.number().optional().default(0),
+  PORT: z.coerce.number().optional().default(3333),
 })
 
 export type Env = z.infer<typeof envSchema>
