f071a6a918c9d8db25d5ee49e5c29cb292a9f2f6
Pedro Duarte <pedrohlduarte@gmail.com>
2024-12-11 09:29:11 -0300

    feat: add vitest dependency to the project for testing improvements

---
+++ vitest.workspace.js
@@ -0,0 +1,9 @@
+
+import { defineWorkspace } from 'vitest/config'
+
+export default defineWorkspace([
+  "./apps/bran/vitest.config.e2e.ts",
+  "./apps/bran/vitest.config.ts",
+  "./apps/bran/dist/vitest.config.js",
+  "./apps/bran/dist/vitest.config.e2e.js"
+])
