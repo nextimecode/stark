ead94c7c49e3421b4d7bd0a53147cc67d8dec3c1
Pedro Duarte <pedrohlduarte@gmail.com>
2024-12-29 12:40:37 -0300

    fix: vitest config

--- apps/bran/vitest.config.ts
+++ apps/bran/vitest.config.ts
@@ -1,13 +1,17 @@
 // vitest.config.ts
 
-import swc from 'unplugin-swc'
 import { defineConfig } from 'vitest/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
+
+import path from 'path';
 
 export default defineConfig({
   test: {
     globals: true,
-    root: './'
+    root: './',
+    deps: {
+      fallbackCJS: true,
+    },
   },
   plugins: [tsConfigPaths()],
   esbuild: {
@@ -16,6 +20,9 @@ export default defineConfig({
     format: 'esm'
   },
   resolve: {
+    alias: {
+      '@/*': path.resolve(__dirname, 'apps/bran/src/*'),
+    },
     mainFields: ['module', 'main', 'jsnext:main'],
     conditions: ['import', 'require'],
     extensions: ['.mjs', '.js', '.ts', '.json']
