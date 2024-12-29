e2380a7550287aa5dd86f832bafd6b3212a91758
Pedro Duarte <pedrohlduarte@gmail.com>
2024-12-29 13:01:49 -0300

    fix: update package.json and vitest configs for ES module support and clean up configuration

--- apps/bran/vitest.config.ts
+++ apps/bran/vitest.config.ts
@@ -1,30 +1,19 @@
-// vitest.config.ts
-
+import swc from 'unplugin-swc'
 import { defineConfig } from 'vitest/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
-
-import path from 'path';
 
 export default defineConfig({
   test: {
     globals: true,
     root: './',
     deps: {
-      fallbackCJS: true,
+      inline: ['vite-tsconfig-paths'],
     },
   },
-  plugins: [tsConfigPaths()],
-  esbuild: {
-    target: 'esnext',
-    loader: 'ts',
-    format: 'esm'
-  },
-  resolve: {
-    alias: {
-      '@/*': path.resolve(__dirname, 'apps/bran/src/*'),
-    },
-    mainFields: ['module', 'main', 'jsnext:main'],
-    conditions: ['import', 'require'],
-    extensions: ['.mjs', '.js', '.ts', '.json']
-  }
+  plugins: [
+    tsConfigPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
 })
