fde9cff0e456f3385055025fd390b1a5827512cc
Pedro Duarte <pedrohlduarte@gmail.com>
2025-01-11 20:06:14 +0000

    feat: remove deprecated vitest workspace configuration file (#31)
    * feat: remove deprecated vitest workspace configuration file
    
    * chore: remove rohan-back subproject from the repository
    
    * chore: remove Vitest configuration files for e2e and general testing
    
    * fix: add .env.test to .gitignore to prevent it from being tracked in version control
    
    * chore: remove redundant start:prod configuration from turbo.json
    
    * fix: update ESLint config to ignore additional build directories and report folders
    
    * fix(eslint): format ignores array for better readability in config files

---
+++ apps/bran/src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common'
+
+import { Either, left, right } from '@/core/either'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+
+import { QuestionDetails } from '../../enterprise/entities/value-objects/question-details'
+import { QuestionsRepository } from '../repositories/questions-repository'
+
+interface GetQuestionBySlugUseCaseRequest {
+  slug: string
+}
+
+type GetQuestionBySlugUseCaseResponse = Either<
+  ResourceNotFoundError,
+  {
+    question: QuestionDetails
+  }
+>
+
+@Injectable()
+export class GetQuestionBySlugUseCase {
+  constructor(private questionsRepository: QuestionsRepository) {}
+
+  async execute({
+    slug
+  }: GetQuestionBySlugUseCaseRequest): Promise<GetQuestionBySlugUseCaseResponse> {
+    const question = await this.questionsRepository.findDetailsBySlug(slug)
+
+    if (!question) {
+      return left(new ResourceNotFoundError())
+    }
+
+    return right({
+      question
+    })
+  }
+}
