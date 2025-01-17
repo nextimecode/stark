e63945c0aeb8c6c744362a9bc40235bd3faaaf49
Pedro Duarte <pedrohlduarte@gmail.com>
2025-01-16 22:52:47 -0300

    chore: comment out unused imports and code in the Home component of the dashboard page

--- apps/sansa/src/app/(dashboard)/(home)/page.tsx
+++ apps/sansa/src/app/(dashboard)/(home)/page.tsx
@@ -1,23 +1,23 @@
-'use client'
+// 'use client'
 
-import { useEffect } from 'react'
+// import { useEffect } from 'react'
 
-import { useRouter } from 'next/navigation'
+// import { useRouter } from 'next/navigation'
 
-import { useAuthContext } from '@/context/AuthContext'
-import { env } from '@/env'
+// import { useAuthContext } from '@/context/AuthContext'
+// import { env } from '@/env'
 
 export default function Home() {
-  const { user } = useAuthContext() as { user: any } // Use 'as' to assert the type as { user: any }
-  const router = useRouter()
+  // const { user } = useAuthContext() as { user: any } // Use 'as' to assert the type as { user: any }
+  // const router = useRouter()
 
-  useEffect(() => {
-    // Redirect to the home page if the user is not logged in
-    if (user == null) {
-      router.push(`${env.NEXT_PUBLIC_ARYA_URL}/`)
-    }
-    // }, [ user ] );
-  }, [user, router]) // Include 'router' in the dependency array to resolve eslint warning
+  // useEffect(() => {
+  //   // Redirect to the home page if the user is not logged in
+  //   if (user == null) {
+  //     router.push(`${env.NEXT_PUBLIC_ARYA_URL}/`)
+  //   }
+  //   // }, [ user ] );
+  // }, [user, router]) // Include 'router' in the dependency array to resolve eslint warning
 
   return (
     <main>
