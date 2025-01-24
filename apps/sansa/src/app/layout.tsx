9101659c78481097b627c569a1f78eeebbdb4fb3
Pedro Duarte <pedrohlduarte@gmail.com>
2025-01-23 22:09:21 -0300

    feat: streamline home page components and enhance layout styles

--- apps/sansa/src/app/layout.tsx
+++ apps/sansa/src/app/layout.tsx
@@ -20,7 +20,7 @@ const inter = Inter({
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html className={inter.variable} lang="pt-BR">
-      <body className="bg-gray-100 dark:bg-neutral-800">
+      <body className="bg-gray-100 dark dark:bg-neutral-800">
         <AuthContextProvider>
           <div className="relative min-h-screen">{children}</div>
         </AuthContextProvider>
