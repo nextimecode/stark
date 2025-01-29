1e3f2ba7e0c96fe8457483b465e5994c389d5fcd
Pedro Duarte <pedrohlduarte@gmail.com>
2025-01-29 20:06:24 -0300

    fix(firebase): use env module for accessing FIREBASE_ADMIN_SERVICE_ACCOUNT variable

--- apps/ned/src/firebase/admin.ts
+++ apps/ned/src/firebase/admin.ts
@@ -1,12 +1,14 @@
 import admin from 'firebase-admin'
 
+import { env } from '@/env'
+
 // Certifique-se de que a variável de ambiente existe
-if (!process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT) {
+if (!env.FIREBASE_ADMIN_SERVICE_ACCOUNT) {
   throw new Error('FIREBASE_ADMIN_SERVICE_ACCOUNT não está definida.')
 }
 
 // Parse na string JSON da variável de ambiente
-const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT)
+const serviceAccount = JSON.parse(env.FIREBASE_ADMIN_SERVICE_ACCOUNT)
 
 if (!admin.apps.length) {
   admin.initializeApp({
