596822d660ae3fa1475e82800ad8fba411be8963
Pedro Duarte <pedrohlduarte@gmail.com>
2025-01-29 20:38:02 -0300

    fix: prevent Firebase Admin from being executed on the client side by adding server check

--- apps/ned/src/firebase/admin.ts
+++ apps/ned/src/firebase/admin.ts
@@ -2,12 +2,14 @@ import admin from 'firebase-admin'
 
 import { env } from '@/env'
 
-// Certifique-se de que a variável de ambiente existe
+if (typeof window !== 'undefined') {
+  throw new Error('O Firebase Admin não pode ser executado no cliente.')
+}
+
 if (!env.FIREBASE_ADMIN_SERVICE_ACCOUNT) {
   throw new Error('FIREBASE_ADMIN_SERVICE_ACCOUNT não está definida.')
 }
 
-// Parse na string JSON da variável de ambiente
 const serviceAccount = JSON.parse(env.FIREBASE_ADMIN_SERVICE_ACCOUNT)
 
 if (!admin.apps.length) {
