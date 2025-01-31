1a7b8ab9ce4e870fdefbe544840e703761c27178
Pedro Duarte <pedrohlduarte@gmail.com>
2025-01-30 21:57:11 -0300

    feat(firebase): refactor Firebase Admin initialization and remove service account key file

--- apps/ned/src/firebase/admin.ts
+++ apps/ned/src/firebase/admin.ts
@@ -1,18 +1,25 @@
-// firebase/admin.ts
+// firebase/admin
 
 import admin from 'firebase-admin'
+
+import { env } from '@/env'
 
 if (typeof window !== 'undefined') {
   throw new Error('O Firebase Admin não pode ser executado no cliente.')
 }
-var serviceAccountKey = require('./serviceAccountKey.json')
 
-// if (!serviceAccountKey) {
-//   throw new Error('FIREBASE_ADMIN_SERVICE_ACCOUNT não está definida.')
-// }
+const serviceAccountKey = env.FIREBASE_ADMIN_SERVICE_ACCOUNT
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountKey)
-})
+if (!serviceAccountKey) {
+  throw new Error('FIREBASE_ADMIN_SERVICE_ACCOUNT não está definida.')
+}
+
+const serviceAccount = JSON.parse(serviceAccountKey)
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  })
+}
 
 export { admin }
