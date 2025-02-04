97cfbd059b1e285fc4d7dc1542a57d19849bea7d
Pedro Duarte <pedrohlduarte@gmail.com>
2025-02-04 15:06:20 -0300

    refactor(mbti): comment out authentication and fetching logic in MyMBTI component

--- apps/sansa/src/app/(private)/(dashboard)/mbti/my-mbti.tsx
+++ apps/sansa/src/app/(private)/(dashboard)/mbti/my-mbti.tsx
@@ -1,40 +1,42 @@
 export const dynamic = 'force-dynamic'
 
+import { api } from '@/data/api'
+
 export async function MyMBTI() {
   // Authenticate and retrieve token
-  const authResponse = await fetch('http://localhost:3333/sessions', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      email: 'diego@rocketseat.com.br',
-      password: '123456'
-    })
-  })
+  // const authResponse = await api('/sessions', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   },
+  //   body: JSON.stringify({
+  //     email: 'diego@rocketseat.com.br',
+  //     password: '123456'
+  //   })
+  // })
 
-  const authData = await authResponse.json()
-  const accessToken = authData.access_token
+  // const authData = await authResponse.json()
+  // const accessToken = authData.access_token
 
-  // Fetch recent questions using retrieved token
-  const response = await fetch('http://localhost:3333/questions?page=1', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${accessToken}`
-    },
-    next: {
-      tags: ['get-tags']
-    }
-  })
+  // const response = await api('/questions?page=1', {
+  //   method: 'GET',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     Authorization: `Bearer ${accessToken}`
+  //   },
+  //   next: {
+  //     tags: ['get-tags']
+  //   }
+  // })
 
-  const { questions } = await response.json()
+  // const { questions } = await response.json()
 
   return (
     <ul>
-      {questions.map((item: any) => (
+      {/* {questions.map((item: any) => (
         <li key={item.id}>{item.slug}</li>
-      ))}
+      ))} */}
+      // TODO: resolver problema do deploy da api
     </ul>
   )
 }
