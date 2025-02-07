91837155e1d360512ae8a6e012fc8293033515a2
Pedro Duarte <pedrohlduarte@gmail.com>
2025-02-07 15:38:49 -0300

    feat: add error handling for authentication and question retrieval in MyMBTI function

--- apps/sansa/src/app/(private)/(dashboard)/mbti/my-mbti.tsx
+++ apps/sansa/src/app/(private)/(dashboard)/mbti/my-mbti.tsx
@@ -6,16 +6,22 @@ export async function MyMBTI() {
   // Authenticate and retrieve token
   const authResponse = await api('/sessions', {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       email: 'diego@rocketseat.com.br',
       password: '123456'
     })
   })
 
-  const authData = await authResponse.json()
+  let authData
+  try {
+    authData = await authResponse.json()
+    console.error('authData', authData)
+  } catch (error) {
+    console.error('Erro ao autenticar:', error)
+    return <p>Erro ao autenticar</p>
+  }
+
   const accessToken = authData.access_token
 
   const response = await api('/questions?page=1', {
@@ -23,13 +29,19 @@ export async function MyMBTI() {
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${accessToken}`
-    },
-    next: {
-      tags: ['get-tags']
     }
   })
+  console.error('questionsData', response)
 
-  const { questions } = await response.json()
+  let questionsData
+  try {
+    questionsData = await response.json()
+  } catch (error) {
+    console.error('Erro ao buscar perguntas:', error)
+    return <p>Erro ao buscar perguntas</p>
+  }
+
+  const { questions } = questionsData
 
   return (
     <ul>{questions?.map((item: any) => <li key={item.id}>{item.slug}</li>)}</ul>
