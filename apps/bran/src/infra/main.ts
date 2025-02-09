ae4e3b2fe5af0ae1a6752fceb1b97b352c9f488d
Pedro Duarte <pedrohlduarte@gmail.com>
2025-02-09 20:00:29 -0300

    fix: update build scripts and file paths in package.json and vercel.json for NestJS app

--- apps/bran/src/infra/main.ts
+++ apps/bran/src/infra/main.ts
@@ -4,16 +4,11 @@ import { AppModule } from './app.module'
 import { EnvService } from './env/env.service'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    // logger: false,
-  })
+  const app = await NestFactory.create(AppModule)
 
   const configService = app.get(EnvService)
   const port = configService.get('PORT')
 
-  if (process.env.VERCEL_ENV === undefined) {
-    await app.listen(port)
-    console.error(`API rodando em http://localhost:${port}`)
-  }
+  await app.listen(port)
 }
 bootstrap()
