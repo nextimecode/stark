8814f9bcfe5ceaee878411e9ad08c3886fec14ad
Pedro Duarte <pedrohlduarte@gmail.com>
2025-02-09 18:50:13 -0300

    fix: remove Swagger configuration and update server startup logic for production environment

--- apps/bran/src/infra/main.ts
+++ apps/bran/src/infra/main.ts
@@ -1,6 +1,4 @@
 import { NestFactory } from '@nestjs/core'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
-
 import { AppModule } from './app.module'
 import { EnvService } from './env/env.service'
 
@@ -9,21 +7,12 @@ async function bootstrap() {
     // logger: false,
   })
 
-  // TODO: precisa configurar o Swagger para ficar automático com o zod
-
-  const config = new DocumentBuilder()
-    .setTitle('Documentação da API')
-    .setDescription('Descrição da API')
-    .setVersion('1.0')
-    .addTag('Bran')
-    .build()
-
-  const documentFactory = () => SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, documentFactory)
-
   const configService = app.get(EnvService)
   const port = configService.get('PORT')
 
-  await app.listen(3000)
+  if (process.env.VERCEL_ENV === undefined) {
+    await app.listen(port);
+    console.error(`API rodando em http://localhost:${port}`);
+  }
 }
 bootstrap()
