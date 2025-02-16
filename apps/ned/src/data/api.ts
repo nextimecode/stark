d7181f317c2b3dc20773ca5a42cc95edf4efffc7
Pedro Duarte <pedrohlduarte@gmail.com>
2025-02-16 19:02:47 -0300

    refactor: remove console.error statements for cleaner code across multiple components

--- apps/ned/src/data/api.ts
+++ apps/ned/src/data/api.ts
@@ -3,14 +3,13 @@ import { env } from '@/env'
 
 export function api(path: string, init?: RequestInit) {
   const baseUrl = env.NEXT_PUBLIC_BRAN_URL
-  console.error('baseUrl', baseUrl)
   if (!baseUrl) {
     throw new Error(
       'A variável de ambiente NEXT_PUBLIC_BRAN_URL não está definida.'
     )
   }
-  // const apiPrefix = '/api'
-  // const url = new URL(apiPrefix.concat(path), baseUrl)
+  const apiPrefix = '/api'
+  const url = new URL(apiPrefix.concat(path), baseUrl)
 
-  return fetch(baseUrl, init)
+  return fetch(url, init)
 }
