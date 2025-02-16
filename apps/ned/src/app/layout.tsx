c1cbf0f15da3a9c46c3e517a213620ea43aa7be1
Pedro Duarte <pedrohlduarte@gmail.com>
2025-02-16 19:07:42 -0300

    fix: reorder imports and update layout for improved structure and readability

--- apps/ned/src/app/layout.tsx
+++ apps/ned/src/app/layout.tsx
@@ -1,29 +1,36 @@
-// app/layout.tsx
-
-'use client'
-
 import { ReactNode } from 'react'
 
+import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 
-import { PrelineScript } from '@/components'
-
-import { AuthContextProvider } from '@/contexts/auth-context'
-
-import './globals.css'
+import { PrelineScript } from '@/components/PrelineScript'
+import './global.css'
 
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter'
 })
 
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1.0,
+  maximumScale: 1.0,
+  userScalable: false
+}
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | NeXTIME',
+    default: 'NeXTIME'
+  },
+  description: ''
+}
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html data-theme="dark" lang="pt-BR">
-      <body className={`${inter.variable}`}>
-        <AuthContextProvider>
-          <div className="relative min-h-screen">{children}</div>
-        </AuthContextProvider>
+      <body className={`${inter.variable} antialiased`}>
+        <div className="relative min-h-screen">{children}</div>
         <PrelineScript />
       </body>
     </html>
