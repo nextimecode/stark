4baad4776bfc036d2d358a43ce1a14a41eb12321
Pedro Duarte <pedrohlduarte@gmail.com>
2025-03-30 17:55:20 -0300

    --no-verify

--- apps/sansa/src/app/(private)/(dashboard)/invite/[subscriberId]/invite-link-input.tsx
+++ apps/sansa/src/app/(private)/(dashboard)/invite/[subscriberId]/invite-link-input.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useState } from 'react'
+
 import { IconButton } from '@/components/icon-button'
 import { InputField, InputIcon, InputRoot } from '@/components/input'
 
@@ -10,8 +12,12 @@ interface InviteLinkInputProps {
 }
 
 export function InviteLinkInput({ inviteLink }: InviteLinkInputProps) {
+  const [tooltipVisible, setTooltipVisible] = useState(false)
+
   function copyInviteLink() {
     navigator.clipboard.writeText(inviteLink)
+    setTooltipVisible(true)
+    setTimeout(() => setTooltipVisible(false), 2000)
   }
 
   return (
@@ -22,9 +28,16 @@ export function InviteLinkInput({ inviteLink }: InviteLinkInputProps) {
 
       <InputField readOnly defaultValue={inviteLink} />
 
-      <IconButton className="-mr-2" onClick={copyInviteLink}>
-        <Copy className="size-5" />
-      </IconButton>
+      <div className="relative">
+        <IconButton className="-mr-2" onClick={copyInviteLink}>
+          <Copy className="size-5" />
+        </IconButton>
+        {tooltipVisible && (
+          <div className="absolute top-0 right-0 transform -translate-y-full bg-gray-700 text-white text-xs py-1 px-2 rounded">
+            Copiado!
+          </div>
+        )}
+      </div>
     </InputRoot>
   )
 }
--- apps/sansa/src/components/icon-button.tsx
+++ apps/sansa/src/components/icon-button.tsx
@@ -8,7 +8,7 @@ export function IconButton({ className, ...props }: ButtonProps) {
   return (
     <button
       className={twMerge(
-        'p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900',
+        'p-1.5 bg-gray-900 text-secondary rounded-md cursor-pointer transition-colors duration-300 hover:bg-gray-900 hover:text-primary',
         className
       )}
       {...props}
