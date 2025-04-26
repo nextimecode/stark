6310b2ee0c81ee452402574306b6eb7d4ae7257e
Pedro Duarte <pedrohlduarte@gmail.com>
2025-04-26 19:54:21 -0300

    refactor: update import paths to use lowercase filenames in file-input component

--- apps/arya/src/components/form/file-input/index.tsx
+++ apps/arya/src/components/form/file-input/index.tsx
@@ -1,5 +1,6 @@
-export { Control, type ControlProps } from './Control'
-export { FileList, type FileListProps } from './FileList'
-export { ImagePreview, type ImagePreviewProps } from './ImagePreview'
-export { Root, type RootProps } from './Root'
-export { Trigger, type TriggerProps } from './Trigger'
+export { Control, type ControlProps } from './control'
+export { FileList, type FileListProps } from './file-list'
+export { ImagePreview, type ImagePreviewProps } from './image-preview'
+export { Root, type RootProps } from './root'
+export { Trigger, type TriggerProps } from './trigger'
+
