331af6762a026b47831eba34443feca4f3eeb9f3
Pedro Duarte <pedrohlduarte@gmail.com>
2025-04-26 20:31:59 -0300

    refactor: update country-select and layout to use new component imports and remove unused sidebar components

--- apps/arya/src/app/(public)/dashboard/country-select.tsx
+++ apps/arya/src/app/(public)/dashboard/country-select.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react'
 import Flag from 'react-world-flags'
 
-import * as Select from '@/components/form/select-teste'
+import * as Select from '@/components/form/select'
 
 import { countries } from '@/lib/countries'
 
--- apps/arya/src/app/(public)/dashboard/layout.tsx
+++ apps/arya/src/app/(public)/dashboard/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react'
 
 import { Inter } from 'next/font/google'
 
-import { Sidebar } from '@/components/sidebar'
+import { Sidebar } from '@/components/sidebar-test'
 
 const inter = Inter({ subsets: ['latin'] })
 
